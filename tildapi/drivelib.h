156f4ab9361fa00cb284066528a943be3c38c721
wangnick <github@semail.wangnick.de>
2013-02-04 13:06:08 +0000

    Attiny now runs at 5V to allow driving HC-SR04 distance sensor from single pin.

--- tildapi/drivelib.h
+++ tildapi/drivelib.h
@@ -1,7 +1,8 @@
 #pragma once
 
 const int MOTOR_POWER_RANGE = 127;
-const double MOTOR_VCC_SCALE = 0.01414; // Using 10k to GND and 33k to VBAT gives 700 units at 9.9V 
+// const double MOTOR_VCC_SCALE = 0.01414; // Using 10k to GND and 33k to VBAT gives 700 units at 9.9V (at 3.3V VCC=AREF)
+const double MOTOR_VCC_SCALE = 0.01927; // Using 10k to GND and 33k to VBAT gives 494 units at 9.52V (at 5V VCC=AREF)
 const double MOTOR_RIGHT_BACKEMF_SPEED = 0.92; // V*s/360°
 const double MOTOR_LEFT_BACKEMF_SPEED = 1.05; // V*s/360°
 const double MOTOR_DIST_MM_SCALE = 1.0/5.8;
